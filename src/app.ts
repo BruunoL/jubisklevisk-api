0e150f1e7736006dc06107131f3eb92d15e7f8d9
BruunoL <brunoluizdevelopment@gmail.com>
2024-04-09 21:14:16 -0300

    feat: Added unit testing and e2e structure

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,30 @@
 import fastify from 'fastify'
+
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUI from "@fastify/swagger-ui";
+
+
 import { env } from '@/env'
 import { ZodError } from 'zod'
 
 export const app = fastify()
+
+app.register(fastifySwagger, {
+  swagger: {
+    consumes: ['application/json'],
+    produces: ['application/json'],
+    info: {
+      title: 'pass.in',
+      description: 'Especificações da API para o back-end da aplicação pass.in construída durante o NLW Unite da Rocketseat.',
+      version: '1.0.0'
+    },
+  }
+  // transform: jsonSchemaTransform,
+})
+
+app.register(fastifySwaggerUI, {
+  routePrefix: '/docs',
+})
 
 app.setErrorHandler((error, _, reply) => {
   if (error instanceof ZodError) {
---
+++ src/use-cases/authenticate.spec.ts
@@ -0,0 +1,7 @@
+import { expect, describe, it, beforeEach } from 'vitest'
+
+describe('Authenticate Use Case', () => {
+  it('should be able to authenticate', async () => {
+    expect("123").toEqual("123")
+  })
+})
